c6554558cee4c7b4060af1b81b6ed71d9e337fb0
Aphiphu <66023029@up.ac.th>
2024-11-23 11:59:04 +0700

    1

--- app/providers/index.tsx
+++ app/providers/index.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from "react";
 
 import { ThemeProvider } from "@wits/next-themes";
 
-// https://github.com/pacocoursey/next-themes/issues/161#issuecomment-1399412698
 export default function Providers({ children }: { children: ReactNode }) {
   return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 }
